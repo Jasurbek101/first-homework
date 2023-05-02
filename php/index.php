cc9389789af6e6ee0b5e8bccb2f6ca5739b4b931
Jasurbek Egamberganov <1jasurbek1693@gmail.com>
2023-05-02 10:27:03 +0500

    done

---
+++ php/index.php
@@ -0,0 +1,52 @@
+<html>
+<?php
+	$name1 = "Sardor";
+	$money1 = 1000;
+	$name2 = "Eshmat";
+	$money2 = 1200;
+	$name3 = "Toshmat";
+	$money3 = 1500;
+ ?>
+<body>
+	<table border="1px" >
+		<tr>
+			<td>
+				<?php
+					echo $name1;
+				  ?>
+			</td>
+			<td>
+				<?php
+					echo $money1;
+				  ?>
+			</td>
+		</tr>
+				<tr>
+			<td>
+				<?php
+					echo $name2;
+				  ?>
+			</td>
+			<td>
+				<?php
+					echo $money2;
+				  ?>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<?php
+					echo $name3;
+				  ?>
+			</td>
+			<td>
+				<?php
+					echo $money3;
+				  ?>
+			</td>
+		</tr>
+	</table>
+</body>
+
+</html>
+
